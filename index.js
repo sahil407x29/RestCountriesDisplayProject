ac1380a4d49e62ef57faa0b924b795b4027f8e47
sahil407x29 <sahil407x29@users.noreply.github.com>
2026-04-30 23:50:04 +0800

    made homescreen

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const cardContainer = document.querySelector('.countries-container')
+
+fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region").then((res) =>
+  res.json()
+).then((data)=> data.forEach((country)=> {
+    
+const countryCard = document.createElement('a')
+countryCard.classList.add('country-card')
+// countryCard.href = 'www.fast.com'gh
+countryCard.innerHTML = `
+  <img src="${country.flags?.svg}" alt="">
+          <div class="card-text">
+            <h3 class="card-title">${country?.name.common}</h3>
+          <p><b>Population : ${country?.population.toLocaleString('en-IN')}</p>
+          <p><b>Region :</b> ${country?.region}</p>
+          <p><b>Capital :</b> ${country?.capital?.join(', ')}</p>
+          `
+cardContainer.appendChild(countryCard)
+console.log(country)
+}))
+
